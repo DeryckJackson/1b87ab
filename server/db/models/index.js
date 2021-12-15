20522bf80db34be598b2fb4177999e08904cda93
Jerax <dgjackon01@gmail.com>
2021-12-15 12:46:48 -0800

    changes to data model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,30 +1,17 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
-const Group = require("./group");
-const GroupUser = require("./groupUser");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.belongsToMany(Conversation, { through: "UserConversations" });
+Conversation.belongsToMany(User, { through: "UserConversations" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
-Message.belongsTo(Group);
-Group.hasMany(Message);
-User.hasMany(GroupUser);
-Group.hasMany(GroupUser);
-GroupUser.belongsTo(User);
-GroupUser.belongsTo(Group);
-Group.belongsTo(User, {
-  foreignKey: "ownerId"
-});
+
 
 module.exports = {
   User,
   Conversation,
   Message,
-  Group,
-  GroupUser
 };
